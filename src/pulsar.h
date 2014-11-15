5bf3ed8389d2afa1933838de4bf945b9f05d6b77
Vladislav Tyulbashev <vtyulb@vtyulb.ru>
2014-11-16 00:06:10 +0300

    more space

--- src/pulsar.h
+++ src/pulsar.h
@@ -56,7 +56,7 @@ struct Pulsar {
             d.push_back(sum / n);
         }
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 100; i++)
             d.push_back(0);
 
         for (int i = firstPoint; i < firstPoint + interval / data.oneStep; i++)

c1cfb36fd1299ecda856c868339fd08fba219dc2
Vladislav Tyulbashev <vtyulb@vtyulb.ru>
2017-03-31 15:40:00 +0300

    installer generation script

---
+++ other/build-installer.bat
@@ -0,0 +1,9 @@
+cd C:\BSA-Analytics\build
+
+git pull
+C:\Qt\qt-5.5.0-x64-mingw510r0-seh-rev0\qt-5.5.0-x64-mingw510r0-seh-rev0\bin\qmake.exe ..\src
+C:\Qt\qt-5.5.0-x64-mingw510r0-seh-rev0\mingw64\bin\mingw32-make.exe -f Makefile.release
+
+move release\BSA-Analytics.exe E:\work\bsa\BSA-Analytics-x64\
+"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" C:\bsa-installation-script-x64.iss
+move C:\Output\BSA-Analytics-x64.exe E:\work\bsa\

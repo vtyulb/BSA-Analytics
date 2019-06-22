6467992ed311d5309f567d922a1d6689d7b06514
Vladislav Tyulbashev <vtyulb@vtyulb.ru>
2019-06-22 15:20:46 +0300

    ssh cipher change

--- other/installer/full-build.sh
+++ other/installer/full-build.sh
@@ -4,4 +4,4 @@ VBoxHeadless -s "Windows 10" &
 sleep 60s
 VBoxManage --nologo guestcontrol "Windows 10" --username "Влад" run --exe "E:\work\bsa\BSA-Analytics\other\installer\build-installer.bat"
 VBoxManage --nologo guestcontrol "Windows 10" --username "Влад" run --exe "E:\work\bsa\BSA-Analytics\other\installer\shutdown.bat"
-scp -P 105 -c aes256-cbc  /home/vlad/work/bsa/BSA-Analytics-x64.exe root@vtyulb.ru:/var/www/public_html/downloads
+scp -P 105  /home/vlad/work/bsa/BSA-Analytics-x64.exe root@vtyulb.ru:/var/www/public_html/downloads
